bbc6cc27207496a42f5126c1510f8d0847ebabec
Olya <ilinskayaov@yandex.ru>
2020-09-07 18:07:28 +0300

    Работающий пэхэпэ🇷🇺
    А также:
    новые цвета
    и новые пробелы!
    И переменную "масло" пришлось переименовать.

--- server.php
+++ server.php
@@ -8,14 +8,15 @@ $r = (float)$_POST['r'];
 function check($x, $y, $r)
 {
     if ((($x >= -$r / 2) && ($x <= 0) && ($y >= 0) && ($y <= $r)) || (($x >= 0) && ($x <= $r) && ($y >= 0) && ($y <= $r) && ($x * $x + $y * $y <= $r * $r)) || (($x >= -$r / 2) && ($x <= 0) && ($y >= -$r / 2) && ($y <= 0) && (-$x - $r / 2 <= $y))) {
-        return "<span style='color: #E8D7FF'>True</span>";
+        return "<span style='color: #7FFF5C'>True</span>";
     } else {
-        return "<span style='color: #FFD7D5'>False</span>";
+        return "<span style='color: #FF47A0'>False</span>";
     }
 }
 
 $result = check($x, $y, $r);
 $now = date("H:i:s");
+$now .="⏰";
 $answer = array($x, $y, $r, check($x, $y, $r), $now, microtime(true) - $start);
 if (!isset($_SESSION['data'])) {
     $_SESSION['data'] = array();
@@ -31,12 +32,14 @@ array_push($_SESSION['data'], $answer);
         <th>Time</th>
         <th>Script time</th>
     </tr>
+    <?php foreach ($_SESSION['data'] as $word) { ?>
     <tr>
-        <td><?php echo $_SESSION['data'][0] ?></td>
-        <td><?php echo $_SESSION['data'][1] ?></td>
-        <td><?php echo $_SESSION['data'][2] ?></td>
-        <td><?php echo $_SESSION['data'][3] ?></td>
-        <td><?php echo $_SESSION['data'][4] ?></td>
-        <td><?php echo number_format($_SESSION['data'][5], 10, ".", "") ?></td>
+        <td><?php echo $word[0] ?></td>
+        <td><?php echo $word[1] ?></td>
+        <td><?php echo $word[2] ?></td>
+        <td><?php echo $word[3] ?></td>
+        <td><?php echo $word[4] ?></td>
+        <td><?php echo number_format($word[5], 10, ".", "") ?></td>
     </tr>
+    <?php }?>
 </table>
